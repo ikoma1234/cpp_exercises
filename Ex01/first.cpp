a9af245824c0751c8e4af27209fac8f8a3855651
Ikoma <ikoshomail@gmail.com>
2020-10-08 11:08:29 +0900

    first

---
+++ Ex01/first.cpp
@@ -0,0 +1,8 @@
+// first.cpp
+#include <iostream>
+
+int main(void) {
+  // COMPLETE: print out "A first c++ program" on stdout using streams
+
+  std::cout << "A first c++ program" << std::endl;
+}
